ba20d49463006b4223991059357017a3231bffe7
Michelle-HP <michellehp@spiralxllc.com>
2025-05-30 14:29:09 -0500

    Update header-footer.js

--- js/header-footer.js
+++ js/header-footer.js
@@ -13,27 +13,17 @@ document.getElementById("header").innerHTML = `
       <img src="./public/Thumbnails/logo.png" alt="image unavailable"/>
       <nav>
         <ul class="nav-links">
-          <li><a href="/esg-mirror/Views/dashboard.php" class="active">Dashboard</a></li>
-          <li><a href="/esg-mirror/Views/epadata.php">Emissions Data</a></li>
-          <li><a href="/esg-mirror/Views/esgmodules.php">ESG Module</a></li>
-          <li><a href="/esg-mirror/Views/documentation.php">Documentation</a></li>
-          <li><a href="/esg-mirror/Views/runstatus.ejs">Run Status</a></li>
-          <li><form action="/users/logout" method="POST">
-            <input type="hidden" name="_method" value="DELETE">
-            <button type="submit" style="align-items: left; padding: 10px 10px; font-weight: bold;">Log out</button>
-           </form></li>
+          <li><a href="index.html" class="active">Home</a></li>
+          <li><a href="product1.html">Product 1</a></li>
+          <li><a href="product2.html">Product 2</a></li>
+          <li><a href="search.html">Search</a></li>
         </ul>
         <div class="menu-icon" onclick="toggleDropdown()" style="color: white;">☰</div>
         <ul class="dropdown" id="dropdown-menu">
-          <li><a href="/esg-mirror/Views/dashboard.php">Dashboard</a></li>
-          <li><a href="/esg-mirror/Views/epadata.php">Emissions Data</a></li>
-          <li><a href="/esg-mirror/Views/esgmodules.php">ESG Module</a></li>
-          <li><a href="/esg-mirror/Views/documentation.php">Documentation</a></li>
-          <li><a href="/esg-mirror/Views/runstatus.ejs">Run Status</a></li>
-          <li><form action="/users/logout" method="POST">
-            <input type="hidden" name="_method" value="DELETE">
-            <button type="submit" style="align-items: left; padding: 10px 10px; font-weight: bold;">Log out</button>
-           </form></li>
+          <li><a href="index.html" class="active">Home</a></li>
+          <li><a href="product1.html">Product 1</a></li>
+          <li><a href="product2.html">Product 2</a></li>
+          <li><a href="search.html">Search</a></li>
         </ul>
       </nav>
     </div>
